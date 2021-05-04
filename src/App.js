b05be60af8a89dbbdb744227e805afec9704dcd5
juancarlosbs <juanrov_@hotmail.com>
2021-05-04 11:59:48 -0300

    feat: params for product detauls

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route } from 'react-router-dom';
 
 import Welcome from './pages/Welcome'
 import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
+
 import MainHeader from './components/MainHeader'
 
 function App() {
@@ -12,9 +14,11 @@ function App() {
         <Route path="/welcome">
           <Welcome />
         </Route>
-
         <Route path="/products">
           <Products />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetail />
         </Route>
       </main>
     </div>
